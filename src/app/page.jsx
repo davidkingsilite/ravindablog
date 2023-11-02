d8c52d3227e39bbe21a866ced877690d57cf4304
Your Name <davidnwachukwu3@gmail.com>
2023-11-02 15:46:25 +0100

    intial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,18 @@
+import styles from './homepage.module.css'
+import Featured from '@/component/featured/Featured';
+import CategoryList from '@/component/CategoryList/CategoryList';
+import CardList from '@/component/CardList/CardList';
+import Menu from '@/component/menu/Menu';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+        <div className={styles.content}>
+          <CardList />
+          <Menu />
+        </div>
+    </div>
+    );
+}
